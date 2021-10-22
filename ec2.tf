89e1e51e3c27269236952a837a1f327d7e47b89f
Prashant <test@test.com>
2021-10-22 10:09:35 +0000

    added ec2 from cli

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "prashant-test" {
+  ami           = ami-02e136e904f3da870
+  instance_type = "t2.nano"
+
+  tags = {
+    Name = "prashant-test2435"
+  }
+}
